c24eeb3e5c3b972c6d2458dccc2b35251ee02805
Jack Brennan <60220030+jackbrennan-creator@users.noreply.github.com>
2022-07-07 17:54:50 +0100

    Add files via upload

--- Examples/parsing_examples/grammar.cs
+++ Examples/parsing_examples/grammar.cs
@@ -1,23 +1,14 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using parsable_objects;
 
 namespace grammar
 {
-  class grammar: parsable, unpasable
+  partial class grammar: parsable
   {
     [Parse(1)] private List<production> productions;
-
-    public void unparse(source_builder source)
-    {
-      int width = productions.Max((p) => ("<" + p.name.spelling + ">").Length);
-      source.add_column(width);
-        source.separate_lines(productions, (p) => p.unparse(source), 2);
-      source.remove_column();
-    }
   }
   
-  class production: parsable, unpasable
+  partial class production: parsable
   {
     [Parse(1, "<")  ] private punctuation       less;
     [Parse(2)       ] public  identifier        name;
@@ -25,112 +16,50 @@ namespace grammar
     [Parse(4, "::=")] private punctuation       defines;
     [Parse(5, "|")  ] private List<alternative> alternatives;
     [Parse(6, ";")  ] private punctuation       semicolon;
-
-    public void unparse(source_builder source)
-    {
-      source.write("<" + name.spelling + ">");
-      source.next_column();
-      
-      source.write(" ::= ");
-      source.separate(alternatives, (a) => a.unparse(source), " | ");
-      source.write(";");
-      
-      source.previous_column();
-    }
   }
   
-  class alternative: parsable, unpasable
+  partial class alternative: parsable
   {
     [Parse(1)] private List<component> components;
-
-    public void unparse(source_builder source)
-    {
-      source.separate(components, (c) => c.unparse(source), " ");
-    }
   }
   
-  class component: parsable, unpasable
+  partial class component: parsable
   {
-    class symbol: component
+    partial class symbol: component
     {
       [Parse(1)] private string spelling;
-
-      public override void unparse(source_builder source)
-      {
-        source.write("\"" + spelling + "\"");
-      }
     }
     
-    class reserved_word: component
+    partial class reserved_word: component
     {
       [Parse(1)] private identifier id;
-
-      public override void unparse(source_builder source)
-      {
-        source.write(id.spelling);
-      }
     }
     
-    class non_terminal: component
+    partial class non_terminal: component
     {
       [Parse(1, "<")  ] private punctuation  less;
       [Parse(2)       ] private identifier   name;
       [Parse(3, ">")  ] private punctuation  greater;
-
-      public override void unparse(source_builder source)
-      {
-        source.write("<");
-        source.write(name.spelling);
-        source.write(">");
-      }
     }
     
-    class option: component
+    partial class option: component
     {
       [Parse(1, "[")] private punctuation sub;
       [Parse(2)     ] private component   component;
       [Parse(3, "]")] private punctuation bus;
-
-      public override void unparse(source_builder source)
-      {
-        source.write("[ ");
-        component.unparse(source);
-        source.write(" ]");
-      }
     }
     
-    class iteration: component
+    partial class iteration: component
     {
       [Parse(1, "{")] private punctuation         open_brace;
       [Parse(2)     ] private component           component;
       [Parse(3)     ] private Optional<separator> separator;
       [Parse(4, "}")] private punctuation         close_brace;
-
-      public override void unparse(source_builder source)
-      {
-        source.write("{ ");
-        component.unparse(source);
-        if (separator.Defined)
-        {
-          source.write(" ");
-          separator.Value.unparse(source);
-        }
-        source.write(" }");
-      }
     }
     
-    class separator: parsable, unpasable
+    partial class separator: parsable
     {
       [Parse(1)] public string spelling;
-
-      public void unparse(source_builder source)
-      {
-        source.write("\"" + spelling + "\"");
-      }
-    }
-
-    public virtual void unparse(source_builder source)
-    {
     }
   }
 }
---
+++ Examples/parsing_examples/grammar_unpaser.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using parsable_objects;
+
+namespace grammar
+{
+  partial class grammar: unpasable
+  {
+    public void unparse(source_builder source)
+    {
+      int width = productions.Max((p) => ("<" + p.name.spelling + ">").Length);
+      source.add_column(width);
+        source.separate_lines(productions, (p) => p.unparse(source), 2);
+      source.remove_column();
+    }
+  }
+  
+  partial class production: unpasable
+  {
+    public void unparse(source_builder source)
+    {
+      source.write("<" + name.spelling + ">");
+      source.next_column();
+      
+      source.write(" ::= ");
+      source.separate(alternatives, (a) => a.unparse(source), " | ");
+      source.write(";");
+      
+      source.previous_column();
+    }
+  }
+  
+  partial class alternative: unpasable
+  {
+    public void unparse(source_builder source)
+    {
+      source.separate(components, (c) => c.unparse(source), " ");
+    }
+  }
+  
+  partial class component: unpasable
+  {
+    partial class symbol: component
+    {
+      public override void unparse(source_builder source)
+      {
+        source.write("\"" + spelling + "\"");
+      }
+    }
+    
+    partial class reserved_word: component
+    {
+      public override void unparse(source_builder source)
+      {
+        source.write(id.spelling);
+      }
+    }
+    
+    partial class non_terminal: component
+    {
+      public override void unparse(source_builder source)
+      {
+        source.write("<");
+        source.write(name.spelling);
+        source.write(">");
+      }
+    }
+    
+    partial class option: component
+    {
+      public override void unparse(source_builder source)
+      {
+        source.write("[ ");
+        component.unparse(source);
+        source.write(" ]");
+      }
+    }
+    
+    partial class iteration: component
+    {
+      public override void unparse(source_builder source)
+      {
+        source.write("{ ");
+        component.unparse(source);
+        if (separator.Defined)
+        {
+          source.write(" ");
+          separator.Value.unparse(source);
+        }
+        source.write(" }");
+      }
+    }
+    
+    partial class separator: unpasable
+    {
+      public void unparse(source_builder source)
+      {
+        source.write("\"" + spelling + "\"");
+      }
+    }
+
+    public virtual void unparse(source_builder source)
+    {
+    }
+  }
+}
+
